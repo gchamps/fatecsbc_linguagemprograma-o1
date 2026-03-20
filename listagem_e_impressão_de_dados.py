dcac1309362134ca2db3816de76ac9ed1c24ad2e
gchamps <gabriel.champin@hotmail.com>
2026-03-19 21:31:27 -0300

    Update listagem_e_impressão_de_dados.py

--- listagem_e_impressão_de_dados.py
+++ listagem_e_impressão_de_dados.py
@@ -1,4 +1,12 @@
+# Imprimir números de 1 a 10
 print("Contando com 'for' em Python:")
-
 for i in range(1,11):
     print(i)
+
+# Lista de nomes 
+nomes = ["Alberto", "Bianca", "Carlos", "Daiana", "Elton"]
+# Imprimir os nomes usando for
+print("\nIterando sobre uma lista em Python:")
+
+for nome in nomes:
+    print(nome)
